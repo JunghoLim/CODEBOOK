c3a79748cf9bdd3e2c49f7973228cdf8e77de5a1
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-02 15:21:08 +0900

    최상위 컴포넌트를 제외한 하위 컴포넌트를 Lazy Loading을 통해 라우팅

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import DefaultIndex from '@/components/DefaultIndex'
+import DefaultIndex from '@/views/DefaultIndex'
 
 Vue.use(VueRouter)
 
+//최상위 컴포넌트인 DefaultIndex 컴포넌트를 제외한 모든 컴포넌트는 Lazy Loading을 사용합니다.
 const routes = [{
     path: '/',
     component: DefaultIndex,
     children: [{
         path: '/',
         name: 'Home',
-        component: Home
+        component: () =>
+            import ( /* webpackChunckName: "default-layout-home" */ '@/layouts/default/Home')
     }]
 }]
 
