fac0956ea4c17da3f591dbacce4d3d207e86f0f6
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-02 14:49:31 +0900

    vuetify 초기 설정

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+//vuetify 기본 프레임워크
+import Vuetify from 'vuetify/lib/framework'
+//컬러 커스텀용 import
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify);
+
+const theme = {
+    //컬러 커스터마이징 부분
+}
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            // light: theme,
+            // dark: theme
+            // 테마 적용 부분
+        }
+    }
+});
