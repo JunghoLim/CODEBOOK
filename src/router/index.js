1c03e5c42fa6d5885a5b5e71b9a7c77c08b82f6f
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-02 15:03:26 +0900

    기본 index 컴포넌트 파일명 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import DefaultIndex from '@/components/HelloWorld'
+import DefaultIndex from '@/components/DefaultIndex'
 
 Vue.use(VueRouter)
 
