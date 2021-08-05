0393ddbe10c9f89a41028e50ec62fba092cdc907
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-05 15:15:07 +0900

    [정호](추가) Member crud를 위한 MemberController 추가 localhost:8090에 대한 CROS 허용

---
+++ src/main/java/com/codebook/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.codebook.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:8090")
+public class MemberController {
+}
