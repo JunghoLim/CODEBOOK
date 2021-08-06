91a22681aa82822ef370d3e50239d4099b5c3e3d
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-06 11:40:40 +0900

    [정호](추가) memberSignUp 회원가입 mapper 코드 작성

---
+++ src/main/java/com/codebook/mapper/MemberMapper.java
@@ -0,0 +1,11 @@
+package com.codebook.mapper;
+
+import com.codebook.dto.Member;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberMapper {
+
+    void memberSignUp(@Param("member") Member member);
+}
