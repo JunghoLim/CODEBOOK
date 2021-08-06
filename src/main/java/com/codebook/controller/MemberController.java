e55cf7f9986907782a5cfa691e823480df7f1e4f
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-06 12:41:32 +0900

    [정호](수정) 회원가입 url 추가 (테스트 완료)

--- src/main/java/com/codebook/controller/MemberController.java
+++ src/main/java/com/codebook/controller/MemberController.java
@@ -1,9 +1,25 @@
 package com.codebook.controller;
 
+import com.codebook.service.MemberService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @CrossOrigin("http://localhost:8090")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/api/member/new")
+    public void memberSignUp(@RequestParam Map<String, String> param){
+        memberService.signUp(param);
+    }
 }
