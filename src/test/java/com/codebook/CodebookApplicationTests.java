dcd6efae17ba793b6f22605626b43b590ab0c3be
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-06 11:39:29 +0900

    [정호](수정) member 추가 테스트 코드 작성

--- src/test/java/com/codebook/CodebookApplicationTests.java
+++ src/test/java/com/codebook/CodebookApplicationTests.java
@@ -1,13 +1,29 @@
 package com.codebook;
 
+import com.codebook.dto.Member;
+import com.codebook.mapper.MemberMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CodebookApplicationTests {
 
+    @Autowired
+    private MemberMapper membermapper;
+
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void memberInsertTest(){
+        Member member = new Member();
+        member.setEmail("asd@asd.com");
+        member.setName("jungho");
+        member.setPassword("12345");
+        membermapper.memberSignUp(member);
     }
 
 }
