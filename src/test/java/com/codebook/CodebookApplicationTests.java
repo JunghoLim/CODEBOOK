25d26b832d7f0b4070e51a2391e077cf2af8c920
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-06 15:25:33 +0900

    [정호](수정) 회원가입 service 메서드 테스트 코드 작성

--- src/test/java/com/codebook/CodebookApplicationTests.java
+++ src/test/java/com/codebook/CodebookApplicationTests.java
@@ -2,9 +2,13 @@ package com.codebook;
 
 import com.codebook.dto.Member;
 import com.codebook.mapper.MemberMapper;
+import com.codebook.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class CodebookApplicationTests {
@@ -12,6 +16,8 @@ class CodebookApplicationTests {
     @Autowired
     private MemberMapper membermapper;
 
+    @Autowired
+    private MemberService memberService;
 
     @Test
     void contextLoads() {
@@ -24,6 +30,15 @@ class CodebookApplicationTests {
         member.setName("jungho");
         member.setPassword("12345");
         membermapper.memberSignUp(member);
+    }
+
+    @Test
+    void serviceMemberSignUpTest(){
+        Map<String, String> testJson = new HashMap<>();
+        testJson.put("email", "zxc@zxc.com");
+        testJson.put("name", "jungho");
+        testJson.put("password", "1234");
+        memberService.signUp(testJson);
     }
 
 }
