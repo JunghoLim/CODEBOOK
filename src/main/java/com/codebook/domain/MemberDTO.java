78863257e504c0be022759fbe06a1d263295c985
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-09 05:48:07 +0900

    [정호](수정) username -> email 재변경

--- src/main/java/com/codebook/domain/MemberDTO.java
+++ src/main/java/com/codebook/domain/MemberDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
-    private String username;    //DB에서는 email이나 security 설정의 편의성을 위해 username으로 사용
+    private String email;
     private String password;
     private String name;
     private String nickName;
