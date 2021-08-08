831acb63da292168b95c16e988fbb36785d9d8ec
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-09 05:28:25 +0900

    [정호](수정) DB Member 테이블의 Entity, email 컬럼의 변수명을 security 설정시 편의를 위하여 username으로 변경

--- src/main/java/com/codebook/domain/MemberDTO.java
+++ src/main/java/com/codebook/domain/MemberDTO.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
-    String email;
-    String password;
-    String name;
+    private String username;    //DB에서는 email이나 security 설정의 편의성을 위해 username으로 사용
+    private String password;
+    private String name;
+    private String nickName;
 }
