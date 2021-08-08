0873f3c19de20a22fef927eb4630f8f43fce37b3
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-09 05:26:30 +0900

    [정호](수정) 서버 통신 테스트용 url 추가, 아이디 중복 검사용 url 추가

--- src/main/java/com/codebook/controller/MemberController.java
+++ src/main/java/com/codebook/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.codebook.controller;
 import com.codebook.service.MemberService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -16,13 +17,28 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    //서버 컨넥팅 테스트용
+    @GetMapping("/test")
+    public String connectTest(@RequestParam("req") String req){
+        return "connecting success your request is" + req;
+    }
+
+    //회원가입
     @PostMapping("/member/new")
     public void memberSignUp(@RequestParam Map<String, String> param){
         memberService.signUp(param);
     }
 
-    @RequestMapping(method = RequestMethod.POST,value = "/api/member/login")
+    //로그인
+    @GetMapping( "/member")
     public Map<String,String> memberLogin(@RequestBody Map<String,String> param){
         return param;
+    }
+
+    //아이디 중복 검사
+    @GetMapping("/member/duplicate")
+    public int memberDuplicateCheck(@RequestParam("email")String email){
+        //반환값이 1이라면 중복 0이면 중복 아님.
+        return memberService.duplicateCheck(email);
     }
 }
