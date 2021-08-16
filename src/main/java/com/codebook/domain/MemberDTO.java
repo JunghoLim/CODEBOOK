2200f9ea792f9fe64c55689cccad9c9515b31dbb
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-16 15:07:49 +0900

    [정호](수정) @ToString 추가 name 필드 삭제 role, status, token 필드 추가

--- src/main/java/com/codebook/domain/MemberDTO.java
+++ src/main/java/com/codebook/domain/MemberDTO.java
@@ -2,11 +2,14 @@ package com.codebook.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Getter @Setter
+@Getter @Setter @ToString
 public class MemberDTO {
     private String email;
     private String password;
-    private String name;
     private String nickname;
+    private String role;
+    private String status;
+    private String token;
 }
