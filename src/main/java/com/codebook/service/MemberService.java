f4bdbb383c547256e2ebf928a74c1772d094fb76
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-16 15:17:57 +0900

    [정호](수정) passwordEncoder new 생성자를 통한 생성 -> 생성자를 통한 의존성 주입 방식 변경

--- src/main/java/com/codebook/service/MemberService.java
+++ src/main/java/com/codebook/service/MemberService.java
@@ -2,25 +2,25 @@ package com.codebook.service;
 
 import com.codebook.domain.MemberDTO;
 import com.codebook.mapper.MemberMapper;
+import com.codebook.security.user.Role;
+import com.codebook.security.user.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberMapper memberMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public void signUp(Map<String, String> param){
-        MemberDTO memberDTO = new MemberDTO();
-        memberDTO.setEmail(param.get("email"));
-        memberDTO.setName(param.get("name"));
-        memberDTO.setPassword(new BCryptPasswordEncoder().encode(param.get("password")));
-        memberDTO.setNickname(param.get("nickname"));
-        memberMapper.memberSignUp(memberDTO);
+    public void signUp(MemberDTO member){
+        member.setPassword(bCryptPasswordEncoder.encode(member.getPassword()));
+        member.setStatus(Status.NONBLOCKED.name());
+        member.setRole(Role.USER.name());
+        member.setToken("");
+        memberMapper.insert_member(member);
     }
 
     public int duplicateCheck(String email){
