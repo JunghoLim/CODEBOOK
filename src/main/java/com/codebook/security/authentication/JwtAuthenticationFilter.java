805147a8d015ce719d53ed1072ab2dbd94e425d4
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-16 15:20:48 +0900

    [정호](추가) usernamePasswordAuthentication을 상속하는 JwtAuthentication 생성 시큐리티 내 로그인 로직 담당 필터로 등록을 위함

---
+++ src/main/java/com/codebook/security/authentication/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.codebook.security.authentication;
+
+import com.codebook.security.user.UserDetailsImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException {
+        ObjectMapper om = new ObjectMapper();
+        try {
+            Map userJson = om.readValue(req.getInputStream(), Map.class);
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userJson.get("username"), userJson.get("password"));
+            return authenticationManager.authenticate(token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authResult.getPrincipal();
+        JwtModel jwtToken = jwtTokenProvider.createToken(userDetails.getUsername());
+        jwtTokenProvider.saveToken(res, jwtToken.getAccessToken());
+    }
+}
