5975c5bc9e407a03dad657a70f99d769b5081295
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-16 15:25:18 +0900

    [정호](추가) Cors 보안 커스텀 설정을 위한 클래스 추가

---
+++ src/main/java/com/codebook/security/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.codebook.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    /*
+    * 응답 JSON 자바스크립트 처리 허용.
+    * 프런트 URL 요청 허용
+    * 헤더에 응답 허용
+    * 모든 방식의 메서드 요청 허용
+    * */
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8090");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+}
