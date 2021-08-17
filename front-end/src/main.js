398672e4411439b5817404f7e46906cef241b920
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-17 17:43:40 +0900

    [정호](수정) 줄바꿈 변경

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -8,9 +8,10 @@ import "./plugins";
 
 Vue.config.productionTip = false;
 Vue.prototype.$Axios = axios;
+
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount("#app");
