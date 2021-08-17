c2a473d2c3cf969c9fa65a91d4ff825b3646c4b9
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-17 17:44:24 +0900

    [정호](수정) store에 member 추가

--- front-end/src/store/modules/index.js
+++ front-end/src/store/modules/index.js
@@ -2,10 +2,12 @@ import app from "./app";
 import board from "./board";
 import boardDetail from "./boardDetail";
 import main_page_boards from "./main_page_boards";
+import member from "./member";
 
 export default {
-  app,
-  board,
-  boardDetail,
-  main_page_boards
+    app,
+    board,
+    boardDetail,
+    main_page_boards,
+    member
 };
