d3d4ffe7d8916a596f63d0024ff9ebd2c8eca25e
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-17 17:43:02 +0900

    [정호](수정) 백엔드 프록시 설정 추가

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+    outputDir: "../src/main/resources/static",
+    indexPath: "../static/index.html",
+    devServer: {
+        proxy: {
+            '/': {
+                "target": 'http://localhost:3000/api',
+                "pathRewrite": { '^/': '' },
+                "changeOrigin": true,
+                "secure": false
+            }
+        }
+    },
     transpileDependencies: [
         'vuetify'
     ]
