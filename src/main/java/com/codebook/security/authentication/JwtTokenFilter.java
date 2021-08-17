c9ea1132b87e4d9e31d81e35f85a42ec25d8ddcb
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-17 17:53:58 +0900

    [정호](수정) 필드 인젝션 -> final 키워드 추가 후 생성자 인젝션으로 변경

--- src/main/java/com/codebook/security/authentication/JwtTokenFilter.java
+++ src/main/java/com/codebook/security/authentication/JwtTokenFilter.java
@@ -5,11 +5,11 @@ import com.codebook.security.user.UserDetailsImpl;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.util.AntPathMatcher;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -20,21 +20,25 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
-public class JwtTokenFilter extends OncePerRequestFilter {
+public class JwtTokenFilter extends BasicAuthenticationFilter {
 
     private static final String[] excludeUrl =  new String[]{
-            "/resources/**","/api/test","/api/member/duplicate","/logout","/api/member/new","/api/member"
+            "/resources/**","/api/member/duplicate","/logout","/api/member/new","/api/member"
     };
 
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberMapper memberMapper;
 
-    @Autowired
-    private MemberMapper memberMapper;
+    public JwtTokenFilter(AuthenticationManager authenticationManager,JwtTokenProvider jwtTokenProvider,MemberMapper memberMapper) {
+        super(authenticationManager);
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.memberMapper = memberMapper;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
         String accessToken = jwtTokenProvider.resolveToken(req);
+        System.out.println(accessToken);
         String refreshToken = null;
 
         //access 토큰 검증
@@ -51,6 +55,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             }
         }catch (Exception e){
             SecurityContextHolder.clearContext();
+            e.printStackTrace();
         }
 
         //refresh 토큰을 통해 access 토큰 재 발급
@@ -59,7 +64,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                     if(jwtTokenProvider.validateToken(refreshToken)){
                         Authentication auth = jwtTokenProvider.getAuthentication(refreshToken);
                         SecurityContextHolder.getContext().setAuthentication(auth);
-
                         // 새로운 access 토큰 발급
                         String newAccessToken = jwtTokenProvider.createToken(jwtTokenProvider.getClaims(refreshToken, "sub")).getAccessToken();
                         jwtTokenProvider.createCookie(res, newAccessToken);
@@ -68,8 +72,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                     //워의 과정에서 예외가 발생했을시 해당 스레드의 security context 를 비움
                     SecurityContextHolder.clearContext();
                 }
-            filterChain.doFilter(req, res);
         }
+            filterChain.doFilter(req, res);
     }
 
     @Override
