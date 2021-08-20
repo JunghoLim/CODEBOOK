222732908be4485b7bba6be50d46d4c237ec3ed1
정연상 <wjddustkd45@gmail.com>
2021-08-20 14:22:48 +0900

    [연상](수정) 로그아웃 부분 수정

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,17 +1,15 @@
 module.exports = {
-    outputDir: "../src/main/resources/static",
-    indexPath: "../static/index.html",
-    devServer: {
-        port: 8090,
-        proxy: {
-            '/api': {
-                "target": 'http://localhost:3000',
-                "changeOrigin": true,
-                "secure": false
-            }
-        }
-    },
-    transpileDependencies: [
-        'vuetify'
-    ],
-}
+  outputDir: "../src/main/resources/static",
+  indexPath: "../static/index.html",
+  devServer: {
+    port: 8090,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/api",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  transpileDependencies: ["vuetify"]
+};
