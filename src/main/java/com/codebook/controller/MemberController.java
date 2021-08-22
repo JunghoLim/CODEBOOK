41104906d0bb0a858c20a1bd3e1123305848cd66
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-23 03:13:32 +0900

    [정호](수정) 서비스 의존성 생성자 주입 어노테이션으로 변경

--- src/main/java/com/codebook/controller/MemberController.java
+++ src/main/java/com/codebook/controller/MemberController.java
@@ -2,22 +2,18 @@ package com.codebook.controller;
 
 import com.codebook.domain.MemberDTO;
 import com.codebook.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
-
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     //서버 컨넥팅 테스트용
     @GetMapping("/test")
@@ -33,14 +29,14 @@ public class MemberController {
 
     //아이디 중복 검사
     @GetMapping("/member/duplicate")
-    public int memberDuplicateCheck(@RequestParam("email")String email){
+    public int memberDuplicateCheck(@RequestParam("email") String email){
         //반환값이 1이라면 중복 0이면 중복 아님.
         return memberService.duplicateCheck(email);
     }
 
     //토큰을 통해 유저의 정보를 추출 후 반환
     @GetMapping("/member")
-    public Map<String, Object> extractMemberDataFromToken(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    public Map<String, Object> extractMemberDataFromToken(HttpServletRequest req, HttpServletResponse res) {
         return memberService.extractMemberData(req, res);
     }
 }
