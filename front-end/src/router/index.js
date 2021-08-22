ecf7851b276dec8d75ff393ec99e3b1806c26b87
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-23 03:11:21 +0900

    [정호](수정) 코드 작성 형식 자동 변경 내용

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -6,86 +6,95 @@ import store from "../store";
 Vue.use(VueRouter);
 
 //최상위 컴포넌트인 DefaultIndex 컴포넌트를 제외한 모든 컴포넌트는 Lazy Loading을 사용합니다.
-const routes = [
-  {
+const routes = [{
     path: "/",
     component: DefaultIndex,
-    children: [
-      {
-        name: "Agreement",
-        path: "/agreement",
-        component: () =>
-          import(
-            /* webpackChunkName: "authentication-agreement" */
-            "@/views/authentication/Agreement"
-          )
-      },
-      {
-        name: "SignUp",
-        path: "/sign-up",
-        component: () =>
-          import(
-            /* webpackChunkName: "views-sign-up" */
-            "@/views/authentication/SignUp"
-          )
-      },
-      {
-        name: "SignIn",
-        path: "/sign-in",
-        component: () =>
-          import(
-            /* webpackChunkName : "views-sign-in" */
-            "@/views/authentication/SignIn"
-          )
-      },
-      {
-        path: "/",
-        name: "Main",
-        component: () =>
-          import(/*webpackChunkName : "views-main" */ "@/views/Main")
-      },
-      {
-        path: "/board-list",
-        name: "BoardList",
-        component: () =>
-          import(
-            /*webpackChunkName : "boards-board-list" */ "@/views/boards/BoardList"
-          )
-      },
-      {
-        path: "/board-detail",
-        name: "BoardDetail",
-        component: () =>
-          import(
-            /*webpackChunkName : "boards-board-list-detail" */ "@/views/boards/BoardDetail"
-          )
-      },
-      {
-        path: "/daily-board",
-        name: "DailyBoard",
-        component: () =>
-          import(
-            /*webpackChunkName : "boards-daliy-board" */ "@/views/boards/DailyBoard"
-          )
-      }
+    children: [{
+            name: "Agreement",
+            path: "/agreement",
+            component: () =>
+                import (
+                    /* webpackChunkName: "authentication-agreement" */
+                    "@/views/authentication/Agreement"
+                )
+        },
+        {
+            path: "/",
+            name: "Main",
+            component: () =>
+                import ( /*webpackChunkName : "views-main" */ "@/views/Main")
+        },
+        {
+            name: "SignUp",
+            path: "/sign-up",
+            component: () =>
+                import (
+                    /* webpackChunkName: "views-sign-up" */
+                    "@/views/authentication/SignUp"
+                )
+        },
+        {
+            name: "SignIn",
+            path: "/sign-in",
+            component: () =>
+                import (
+                    /* webpackChunkName : "views-sign-in" */
+                    "@/views/authentication/SignIn"
+                )
+        },
+        {
+            path: "/board-list",
+            name: "BoardList",
+            component: () =>
+                import (
+                    /*webpackChunkName : "boards-board-list" */
+                    "@/views/boards/BoardList"
+                )
+        },
+        {
+            path: "/board-detail",
+            name: "BoardDetail",
+            component: () =>
+                import (
+                    /*webpackChunkName : "boards-board-list-detail" */
+                    "@/views/boards/BoardDetail"
+                )
+        },
+        {
+            path: "/daily-board",
+            name: "DailyBoard",
+            component: () =>
+                import (
+                    /*webpackChunkName : "boards-daliy-board" */
+                    "@/views/boards/DailyBoard"
+                )
+        },
+        {
+            path: "/profile",
+            name: "Profile",
+            component: () =>
+                import (
+                    /* webpackChunkName : "member-profile" */
+                    "@/views/member/Profile"
+                )
+        }
     ]
-  }
-];
+}];
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.name === "SignUp") {
-    if (store.getters["app/getAgreement"]) {
-      next();
+    if (to.name === "SignUp") {
+        if (store.getters["app/getAgreement"]) {
+            next();
+        } else {
+            next("/agreement");
+        }
     } else {
-      next("/agreement");
+        next();
     }
-  } else {
-    next();
-  }
 });
 export default router;
