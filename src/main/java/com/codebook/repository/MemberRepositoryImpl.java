fe02403bc06a46a2452e8d2eb1b424e66be25d92
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-25 17:36:18 +0900

    [정호](수정) 받아온 이미지 파일에 대한 정보를 사진 테이블에 저장, 프로필 이미지를 변경한것임으로 프로필의 해당 이미지로 업데이트

--- src/main/java/com/codebook/repository/MemberRepositoryImpl.java
+++ src/main/java/com/codebook/repository/MemberRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.codebook.repository;
 
 import com.codebook.domain.MemberDTO;
+import com.codebook.domain.UploadImgFileDTO;
 import com.codebook.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -15,5 +16,11 @@ public class MemberRepositoryImpl implements MemberRepository{
     public void insertMemberAndProfile(MemberDTO memberDTO) {
         memberMapper.insert_member(memberDTO);
         memberMapper.insert_profile(memberDTO);
+    }
+
+    @Override
+    public void updateMemberProfileImg(UploadImgFileDTO uploadImgFile) {
+        memberMapper.insert_picture(uploadImgFile);
+        memberMapper.update_ProfileImg(uploadImgFile);
     }
 }
