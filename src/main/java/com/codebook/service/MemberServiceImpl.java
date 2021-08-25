5360b1cbc3f4a82944e24479568bc601838d4ddd
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-25 17:39:30 +0900

    [정호](수정) 권한을 받아오는 로직을 메서드화, 이미지 파일 반환 메서드, 프로필 업데이트 메서드 구현

--- src/main/java/com/codebook/service/MemberServiceImpl.java
+++ src/main/java/com/codebook/service/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package com.codebook.service;
 
 import com.codebook.domain.MemberDTO;
 import com.codebook.domain.MemberProfileDTO;
+import com.codebook.domain.UploadImgFileDTO;
+import com.codebook.handler.ProfileImgFileHandler;
 import com.codebook.mapper.MemberMapper;
 import com.codebook.repository.MemberRepository;
 import com.codebook.security.authentication.token.JwtTokenProvider;
@@ -10,12 +12,17 @@ import com.codebook.security.user.Status;
 import com.codebook.security.user.UserDetailsImpl;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,6 +34,19 @@ public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
+    private final ProfileImgFileHandler profileImgFileHandler;
+
+    public Authentication getAuth(HttpServletRequest req, HttpServletResponse res){
+        String token = jwtTokenProvider.resolveTokenFromRequest(req);
+        Authentication auth = null;
+        try{
+            auth = jwtTokenProvider.getAuthentication(token);
+        }catch (ExpiredJwtException e){
+            token = jwtTokenProvider.resolveTokenFromResponse(res);
+            auth = jwtTokenProvider.getAuthentication(token);
+        }
+        return auth;
+    }
 
     @Override
     public void signUp(MemberDTO member){
@@ -44,14 +64,7 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public Map<String, Object> extractMemberData(HttpServletRequest req, HttpServletResponse res) {
-        String token = jwtTokenProvider.resolveTokenFromRequest(req);
-        Authentication auth = null;
-        try{
-            auth = jwtTokenProvider.getAuthentication(token);
-        }catch (ExpiredJwtException e){
-            token = jwtTokenProvider.resolveTokenFromResponse(res);
-            auth = jwtTokenProvider.getAuthentication(token);
-        }
+        Authentication auth = getAuth(req, res);
         UserDetailsImpl member = (UserDetailsImpl) auth.getPrincipal();
         MemberProfileDTO profile = memberMapper.findProfileByEmail(member.getUsername());
         Map<String, Object> memberData = new HashMap<>();
@@ -62,6 +75,25 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public void updateProfile(MemberProfileDTO profile) {
-        memberMapper.update_Profile(profile);
+        memberMapper.update_ProfileInfo(profile);
     }
+
+    @Override
+    public void updateProfileImg(HttpServletRequest req, HttpServletResponse res, MultipartFile file) throws IOException {
+        Authentication auth = getAuth(req, res);
+        UserDetailsImpl member = (UserDetailsImpl) auth.getPrincipal();
+        UploadImgFileDTO uploadImgFile = profileImgFileHandler.parseImgFile(member.getUsername(),file);
+        memberRepository.updateMemberProfileImg(uploadImgFile);
+    }
+
+    @Override
+    public ResponseEntity<byte[]> getProfileImg(String imgName) throws IOException {
+        String absolutePath = new File("").getAbsolutePath() + "\\";
+        String path = "./src/main/resources/static/uploadedfiles/images/";
+        InputStream imageStream = new FileInputStream(absolutePath + path + imgName);
+        byte[] imageByteArray = IOUtils.toByteArray(imageStream);
+        imageStream.close();
+        return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
+    }
+
 }
