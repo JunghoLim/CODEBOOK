3a1b7d1e80a8edea207bd8b3f39f8799d954679b
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-29 05:39:43 +0900

    [정호](수정) c -> cnt 로 변수명 수정

--- src/main/java/com/codebook/domain/BoardDTO.java
+++ src/main/java/com/codebook/domain/BoardDTO.java
@@ -11,8 +11,9 @@ public class BoardDTO {
     private String writedate;//작성일
     private String email;//이메일
     private String content;
+    private String category;
     private int views;//조회수
     private int recommend;//추천수
-    private int comment_c;//댓글 수를 comment_count하면 길어서 c로 줄였습니다.
-    private int report_c;
+    private int comment_cnt;
+    private int report_cnt;
 }
