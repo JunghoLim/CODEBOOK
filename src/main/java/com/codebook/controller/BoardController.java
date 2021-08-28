5a5a58c46ee932f8b414c13e734dc9dbfebf0ead
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-29 05:39:02 +0900

    [정호](수정) 검색 혹은 카테고리를 통해 데이터를 받아오도록 수정

--- src/main/java/com/codebook/controller/BoardController.java
+++ src/main/java/com/codebook/controller/BoardController.java
@@ -1,14 +1,12 @@
 package com.codebook.controller;
 
 
-import com.codebook.domain.BoardDTO;
-import com.codebook.domain.CommentDTO;
 import com.codebook.service.BoardService;
-import org.apache.ibatis.annotations.Param;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,18 +21,25 @@ public class BoardController {//게시판 정보 가져오는....
     }
 
     @GetMapping("/board/list")
-    public Map<String, Object> board_view(@Param("page") int page){
+    public Map<String, Object> board_view(@PathParam("page") int page, @PathParam("category") String category, @PathParam("searchText") String searchText){
         if(page==0){
             page = 1;
         }
+        int pagination = 0;
         Map<String,Object> board = new HashMap<>();
-        Integer pagination = boardService.page_per_data();
-        board.put("board_list",boardService.board_list(page));
+        if(StringUtils.isEmpty(category) && StringUtils.isNotEmpty(searchText)){
+            pagination = boardService.page_per_data_search(searchText);
+            board.put("board_list", boardService.board_list_search(page, searchText));
+        }else {
+            pagination = boardService.page_per_data(category);
+            board.put("board_list", boardService.board_list(page, category));
+        }
         board.put("pagination", pagination);
+        board.put("category", category);
         return board;
     }
     @GetMapping("/board")
-    public Map<String,Object> board_detail(@Param("bno")int bno){
+    public Map<String,Object> board_detail(@PathParam("bno")int bno){
         Map<String,Object> board_detail = new HashMap<>();
 
         board_detail.put("board_detail",boardService.board_detail(bno));
@@ -43,7 +48,7 @@ public class BoardController {//게시판 정보 가져오는....
 
     }
     @GetMapping("/board/comment")
-        public Map<String,Object> board_comment(@Param("bno")int bno){
+        public Map<String,Object> board_comment(@PathParam("bno")int bno){
             Map<String,Object> comment = new HashMap<>();
             comment.put("comment_list",boardService.comment(bno));
             return comment;
