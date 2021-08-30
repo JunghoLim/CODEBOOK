0012ed4453ed788f8ad38e020eff825d39614a8b
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-30 17:37:31 +0900

    [정호](수정) 프로필에서 자신의 쓴 게시글을 보기위한 board_list_search_byEmail , 페이지네이션용 page_per_data_search_byEamil, 메인화면 데이터 출력용 getMainBoards 추가

--- src/main/java/com/codebook/service/BoardService.java
+++ src/main/java/com/codebook/service/BoardService.java
@@ -7,6 +7,7 @@ import com.codebook.mapper.CommentMapper;
 import org.springframework.stereotype.Service;
 
 import javax.xml.stream.events.Comment;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -38,12 +39,26 @@ public class BoardService {
         return boardMapper.page_per_board_search(start_row,end_row, searchText);
     }
 
+    public List<BoardDTO> board_list_search_byEmail(int page, String searchText){
+        int all_data = boardMapper.all_board_search_byEmail(searchText);
+        int start_row= page*10-9;
+        int end_row= start_row+9;
+        if( end_row > all_data){
+            end_row = all_data;
+        }
+        return boardMapper.page_per_board_search_byEmail(start_row,end_row, searchText);
+    }
+
     public int page_per_data(String category){
         return boardMapper.all_board(category)%10==0 ? boardMapper.all_board(category)/10 : (boardMapper.all_board(category)/10)+1;
     }
 
     public int page_per_data_search(String searchText){
         return boardMapper.all_board_search(searchText)%10==0 ? boardMapper.all_board_search(searchText)/10 : (boardMapper.all_board_search(searchText)/10)+1;
+    }
+
+    public int page_per_data_search_byEmail(String searchText){
+        return boardMapper.all_board_search_byEmail(searchText)%10==0 ? boardMapper.all_board_search_byEmail(searchText)/10 : (boardMapper.all_board_search_byEmail(searchText)/10)+1;
     }
 
     public BoardDTO board_detail(int bno){return boardMapper.board_detail(bno);}
@@ -56,5 +71,14 @@ public class BoardService {
         int result = boardMapper.insert_comment(comment,bno,email);
         if(result==1) return result;
         else return 0;
+    }
+
+    public Map<String, Object> getMainBoards(){
+        Map<String, Object> mainBoards = new HashMap<>();
+        mainBoards.put("weeklyBestBoards", boardMapper.select_main_termBoard(7, 6));
+        mainBoards.put("monthlyBestBoards", boardMapper.select_main_termBoard(30, 3));
+        mainBoards.put("latestBoards", boardMapper.select_main_latestBoard());
+        mainBoards.put("mostFollowedMember", boardMapper.select_main_profile());
+        return mainBoards;
     }
 }
