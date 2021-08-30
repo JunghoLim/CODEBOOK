d1410e15f07188d426889f15445610fa36e58bce
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-30 17:23:36 +0900

    [정호](수정) 보드 리스트 메서드 파라미터의 데이터 값 존재여부, 값 분석 후 서비스의 메서드 실행

--- src/main/java/com/codebook/controller/BoardController.java
+++ src/main/java/com/codebook/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.codebook.controller;
 
 
+import com.codebook.mapper.BoardMapper;
 import com.codebook.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,25 +14,28 @@ import java.util.Map;
 @RestController
 @CrossOrigin("http://localhost:8090")
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class BoardController {//게시판 정보 가져오는....
 
     private final BoardService boardService;
 
-    public BoardController(BoardService boardService){
-        this.boardService = boardService;
-    }
-
     @GetMapping("/board/list")
-    public Map<String, Object> board_view(@PathParam("page") int page, @PathParam("category") String category, @PathParam("searchText") String searchText){
-        if(page==0){
+    public Map<String, Object> board_view(@PathParam("page") int page, @PathParam("category") String category, @PathParam("searchText") String searchText) {
+        if (page == 0) {
             page = 1;
         }
         int pagination = 0;
-        Map<String,Object> board = new HashMap<>();
-        if(StringUtils.isEmpty(category) && StringUtils.isNotEmpty(searchText)){
-            pagination = boardService.page_per_data_search(searchText);
-            board.put("board_list", boardService.board_list_search(page, searchText));
-        }else {
+        Map<String, Object> board = new HashMap<>();
+        if (StringUtils.isEmpty(category) && StringUtils.isNotEmpty(searchText)) {
+            String[] isEmail = searchText.split("_");
+            if (isEmail[0].equals("email")) {
+                pagination = boardService.page_per_data_search_byEmail(isEmail[1]);
+                board.put("board_list", boardService.board_list_search_byEmail(page, isEmail[1]));
+            } else {
+                pagination = boardService.page_per_data_search(searchText);
+                board.put("board_list", boardService.board_list_search(page, searchText));
+            }
+        } else {
             pagination = boardService.page_per_data(category);
             board.put("board_list", boardService.board_list(page, category));
         }
@@ -38,30 +43,36 @@ public class BoardController {//게시판 정보 가져오는....
         board.put("category", category);
         return board;
     }
-    @GetMapping("/board")
-    public Map<String,Object> board_detail(@PathParam("bno")int bno){
-        Map<String,Object> board_detail = new HashMap<>();
 
-        board_detail.put("board_detail",boardService.board_detail(bno));
+    @GetMapping("/board")
+    public Map<String, Object> board_detail(@PathParam("bno") int bno) {
+        Map<String, Object> board_detail = new HashMap<>();
+
+        board_detail.put("board_detail", boardService.board_detail(bno));
 
         return board_detail;
 
     }
+
     @GetMapping("/board/comment")
-        public Map<String,Object> board_comment(@PathParam("bno")int bno){
-            Map<String,Object> comment = new HashMap<>();
-            comment.put("comment_list",boardService.comment(bno));
-            return comment;
-        }
+    public Map<String, Object> board_comment(@PathParam("bno") int bno) {
+        Map<String, Object> comment = new HashMap<>();
+        comment.put("comment_list", boardService.comment(bno));
+        return comment;
+    }
 
     @PostMapping("/comment")
-    public int input_comment(@RequestBody Map<String,Object> param){
-        int bno = (int)param.get("bno");
-        String email = (String)param.get("email");
-        String comment = (String)param.get("comment");
-
-        int result = boardService.insert_comment(comment,bno,email);
-        return result;
-        }
+    public int input_comment(@RequestBody Map<String, Object> param) {
+        int bno = (int) param.get("bno");
+        String email = (String) param.get("email");
+        String comment = (String) param.get("comment");
+        return boardService.insert_comment(comment, bno, email);
     }
+
+    @GetMapping("/board/main/list")
+    public Map<String, Object> mainBoards() {
+        return boardService.getMainBoards();
+    }
+}
+
 
