3f3a7ec4c7b408dca9c937c0c302ed6f21296769
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-30 17:19:25 +0900

    [정호](수정) 컴포넌트 최종본

--- front-end/src/views/Search.vue
+++ front-end/src/views/Search.vue
@@ -15,6 +15,7 @@
               :headers="board_headers"
               :items="boardData"
               hide-default-footer
+              no-data-text="비슷한 제목을 가진 게시글이 존재하지 않아요!"
             />
           </v-card-text>
           <template class="mt-10">
@@ -37,23 +38,50 @@
           class="px-5 py-3 text-center"
         >
           <v-divider />
-          <template
-            v-for="profile in profiles"
-          >
-            <profile-card
-              :key="profile.email"
-              :email="profile.email"
-              :nickname="profile.nickname"
-              :about-me="profile.aboutMe"
-              :avatar="'/api/member/profile/img/'+profile.picturePath"
-            />
-          </template>
+          <v-row>
+            <template
+              v-if="profiles.length == 0"
+            >
+              <v-col cols="12`">
+                <v-card outlined>
+                  <v-container class="py-8">
+                    <div class="d-flex justify-center mb-4">
+                      <v-icon size="40">
+                        mdi-emoticon-sad-outline
+                      </v-icon>
+                    </div>
+
+                    <p class="text-h4 text-center my-0 py-0 grey--text text--darken-3">
+                      Oops! 비슷한 닉네임을 가진 사용자가 존재하지 않아요!
+                    </p>
+                  </v-container>
+                </v-card>
+              </v-col>
+            </template>
+            <template
+              v-for="profile in profiles"
+              v-else
+            >
+              <v-col
+                :key="profile.email"
+                cols="3"
+                md="6"
+                sm="12"
+              >
+                <profile-card
+                  :email="profile.email"
+                  :nickname="profile.nickname"
+                  :about-me="profile.aboutMe"
+                  :avatar="'/api/member/profile/img/'+profile.picturePath"
+                />
+              </v-col>
+            </template>
+          </v-row>
         </material-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
 <script>
 export default {
   name:'Search',
@@ -63,9 +91,6 @@ export default {
   },
   data: () => ({
     searchText: '',
-    memberPage: 1,
-    boardPage: 1,
-    memberPagination: 1,
     boardPagination: 1,
     board_headers: [
           {
@@ -114,18 +139,6 @@ export default {
             align: 'right',
           },
         ],
-        boards: [
-          {
-            bno: 1,
-            nickname: 'jungho',
-            title: '첫번째 개시글',
-            views: 0,
-            recommends: 0,
-            comments: 0,
-            writeDate: '2020-20-20',
-            category: 'free'
-          }
-        ],
         profiles:[]
   }),
   computed:{
@@ -133,12 +146,20 @@ export default {
       get() {
         return this.$store.getters["board/getBoardData"];
       }
+    },
+    boardPage:{
+      get(){
+        return Number(this.$route.params.page);
+      },
+      set(){
+        return Number(this.$route.params.page);
+      }
     }
   },
   created(){
-    this.searchText = this.$route.query.searchText
+    this.searchText = this.$route.params.searchText;
     axios
-      .get("/api/board/list", { params: { 'page': this.boardPage, 'searchText': this.searchText } })
+      .get("/api/board/list", { params: { 'page': this.$route.params.page, 'searchText': this.searchText } })
       .then(res => {
         let result = res.data.board_list;
         this.boardPagination = res.data.pagination;
