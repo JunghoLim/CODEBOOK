1520fb5f41b2902098a33b1babe08ecb9ac3711e
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-08-31 16:21:48 +0900

    [정호](수정) text editor 테스트용 컴퍼넌트

---
+++ front-end/src/views/boards/BoardWrite.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <button @click="handleSavingContent">
+      전송
+    </button>
+    <vue-editor v-model="content" />
+  </div>
+</template>
+
+<script>
+import { VueEditor } from "vue2-editor";
+
+export default {
+  components: {
+    VueEditor
+  },
+
+  data() {
+    return {
+      content: "<h3>Initial Content</h3>"
+    };
+  },
+
+  methods: {
+    handleSavingContent: function() {
+      // You have the content to save
+      console.log(this.content);
+    }
+  }
+};
+</script>
