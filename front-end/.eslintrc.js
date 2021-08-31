1cb0945a0125a5b87b09f95c7e63231ed449acc8
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-09-01 07:35:12 +0900

    [정호](수정) vue text editor 라이브러리 설치

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'vuetify/no-deprecated-classes': 'error',
-        'vuetify/no-legacy-grid': 'error'
+        'vuetify/no-legacy-grid': 'error',
     }
 }
