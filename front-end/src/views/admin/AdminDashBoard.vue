5d31694b92bae4ae5f248cdcae16199fec0585ee
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-09-03 03:32:00 +0900

    [정호](수정) 전달받을 데이터 수정

--- front-end/src/views/admin/AdminDashBoard.vue
+++ front-end/src/views/admin/AdminDashBoard.vue
@@ -8,131 +8,14 @@
     <v-row>
       <v-col
         cols="12"
-        lg="6"
-      >
-        <material-chart-card
-          :data="emailsSubscriptionChart.data"
-          :options="emailsSubscriptionChart.options"
-          :responsive-options="emailsSubscriptionChart.responsiveOptions"
-          color="#E91E63"
-          hover-reveal
-          type="Bar"
-        >
-          <template v-slot:reveal-actions>
-            <v-tooltip bottom>
-              <template v-slot:activator="{ attrs, on }">
-                <v-btn
-                  v-bind="attrs"
-                  color="info"
-                  icon
-                  v-on="on"
-                >
-                  <v-icon
-                    color="info"
-                  >
-                    mdi-refresh
-                  </v-icon>
-                </v-btn>
-              </template>
-
-              <span>Refresh</span>
-            </v-tooltip>
-
-            <v-tooltip bottom>
-              <template v-slot:activator="{ attrs, on }">
-                <v-btn
-                  v-bind="attrs"
-                  light
-                  icon
-                  v-on="on"
-                >
-                  <v-icon>mdi-pencil</v-icon>
-                </v-btn>
-              </template>
-
-              <span>Change Date</span>
-            </v-tooltip>
-          </template>
-
-          <h4 class="card-title font-weight-light mt-2 ml-2">
-            월별 방문자 수
-          </h4>
-
-          <p class="d-inline-flex font-weight-light ml-2 mt-1">
-            <percentage-arrow :data="-time" />
-            전월 대비 방문율
-          </p>
-        </material-chart-card>
-      </v-col>
-
-      <v-col
-        cols="12"
-        lg="6"
-      >
-        <material-chart-card
-          :data="dailySalesChart.data"
-          :options="dailySalesChart.options"
-          color="success"
-          hover-reveal
-          type="Line"
-        >
-          <template v-slot:reveal-actions>
-            <v-tooltip bottom>
-              <template v-slot:activator="{ attrs, on }">
-                <v-btn
-                  v-bind="attrs"
-                  color="info"
-                  icon
-                  v-on="on"
-                >
-                  <v-icon
-                    color="info"
-                  >
-                    mdi-refresh
-                  </v-icon>
-                </v-btn>
-              </template>
-
-              <span>Refresh</span>
-            </v-tooltip>
-
-            <v-tooltip bottom>
-              <template v-slot:activator="{ attrs, on }">
-                <v-btn
-                  v-bind="attrs"
-                  light
-                  icon
-                  v-on="on"
-                >
-                  <v-icon>mdi-pencil</v-icon>
-                </v-btn>
-              </template>
-
-              <span>Change Date</span>
-            </v-tooltip>
-          </template>
-
-          <h4 class="card-title font-weight-light mt-2 ml-2">
-            월별 게시글 수
-          </h4>
-
-          <p class="d-inline-flex font-weight-light ml-2 mt-1">
-            <percentage-arrow :data="time" />
-            전월 대비 개시글 작성률
-          </p>
-        </material-chart-card>
-      </v-col>
-
-      <v-col
-        cols="12"
         sm="6"
         lg="3"
       >
         <material-stats-card
           color="info"
           icon="mdi-account"
-          title="일일 방문자 수"
-          value="+245"
+          title="등록 회원수"
+          :value="memberCnt"
         />
       </v-col>
 
@@ -143,9 +26,9 @@
       >
         <material-stats-card
           color="primary"
-          icon="mdi-account-multiple"
-          title="주간 방문자 수"
-          value="75.521"
+          icon="mdi-account-alert"
+          title="정지 회원 수"
+          :value="blockedMemberCnt"
         />
       </v-col>
 
@@ -158,7 +41,7 @@
           color="success"
           icon="mdi-clipboard-text"
           title="일일 개시글 수"
-          value="$ 34,245"
+          :value="todayBoardCnt"
         />
       </v-col>
 
@@ -171,7 +54,7 @@
           color="orange"
           icon="mdi-clipboard-text-multiple"
           title="주간 개시글 수"
-          value="184"
+          :value="weeklyBoardCnt"
         />
       </v-col>
 
@@ -196,8 +79,14 @@
           </template>
           <v-card-text>
             <v-data-table
-              :headers="headers"
+              :headers="member_headers"
               :items="items"
+              hide-default-footer
+            />
+            <v-pagination
+              v-model="memberPage"
+              :length="memberPagination"
+              @input="pageNext"
             />
           </v-card-text>
         </material-card>
@@ -228,6 +117,11 @@
               :items="boards"
               hide-default-footer
             />
+            <v-pagination
+              v-model="boardPage"
+              :length="boardPagination"
+              @input="pageNext"
+            />
           </v-card-text>
         </material-card>
       </v-col>
@@ -248,21 +142,15 @@
               >Tasks:</span>
               <v-tab class="mr-3">
                 <v-icon class="mr-2">
-                  mdi-bug
-                </v-icon>
-                Bugs
-              </v-tab>
-              <v-tab class="mr-3">
-                <v-icon class="mr-2">
                   mdi-code-tags
                 </v-icon>
-                Website
+                건의
               </v-tab>
               <v-tab>
                 <v-icon class="mr-2">
                   mdi-cloud
                 </v-icon>
-                Server
+                공지
               </v-tab>
             </v-tabs>
           </template>
@@ -281,16 +169,7 @@
                     :key="i"
                     align="center"
                   >
-                    <v-col cols="1">
-                      <v-list-item-action>
-                        <v-checkbox
-                          v-model="task.value"
-                          color="secondary"
-                        />
-                      </v-list-item-action>
-                    </v-col>
-
-                    <v-col cols="9">
+                    <v-col cols="10">
                       <div
                         class="font-weight-light"
                         v-text="task.text"
@@ -307,6 +186,7 @@
                       <v-icon
                         color="error"
                         class="mx-1"
+                        @click="alert"
                       >
                         mdi-close
                       </v-icon>
@@ -323,115 +203,46 @@
 </template>
 
 <script>
-import MaterialChartCard from '@/components/MaterialChartCard'
 import MaterialStatsCard from '@/components/MaterialStatsCard'
 import MaterialCard from '@/components/MaterialCard'
-import PercentageArrow from '@/components/PercentageArrow'
   export default {
     name: 'AdminDashboard',
     components:{
-      MaterialChartCard,
       MaterialStatsCard,
       MaterialCard,
-      PercentageArrow
     },
     data () {
       return {
-        time: -10,
-        dailySalesChart: {
-          data: {
-            labels: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-            series: [
-              [3122, 4431, 3206, 7808, 5533, 4532, 3268, 4345, 5685, 6108, 1222, 8952],
-            ],
-          },
-          options: {
-            lineSmooth: this.$chartist.Interpolation.cardinal({
-              tension: 30,
-            }),
-            low: 0,
-            high: 10000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-            },
-          },
-        },
-        dataCompletedTasksChart: {
-          data: {
-            labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-            series: [
-              [230, 750, 450, 300, 280, 240, 200, 190],
-            ],
-          },
-          options: {
-            lineSmooth: this.$chartist.Interpolation.cardinal({
-              tension: 0,
-            }),
-            low: 0,
-            high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-            },
-          },
-        },
-        emailsSubscriptionChart: {
-          data: {
-            labels: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-            series: [
-              [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-
-            ],
-          },
-          options: {
-            axisX: {
-              showGrid: false,
-            },
-            low: 0,
-            high: 1000,
-            chartPadding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-            },
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0]
-                },
-              },
-            }],
-          ],
-        },
-        headers: [
+        memberCnt: 0,
+        blockedMemberCnt: 0,
+        todayBoardCnt: 0,
+        weeklyBoardCnt: 0,
+        member_headers: [
           {
             sortable: false,
-            text: 'Nickname',
+            text: '닉네임',
             value: 'nickname',
           },
           {
             sortable: false,
-            text: 'Email',
+            text: '이메일',
             value: 'email',
           },
           {
             sortable: false,
-            text: 'Posts',
-            value: 'posts',
+            text: '게시글수',
+            value: 'boardCnt',
             align: 'right',
           },
           {
             sortable: false,
-            text: 'Status',
+            text: '팔로워',
+            value: 'follower',
+            align: 'right',
+          },
+          {
+            sortable: false,
+            text: '상태',
             value: 'status',
             align: 'right',
           },
@@ -439,48 +250,46 @@ import PercentageArrow from '@/components/PercentageArrow'
         board_headers: [
           {
             sortable: false,
-            text: 'Nickname',
-            value: 'nickname',
+            text: '번호',
+            value: 'bno',
           },
           {
-            sortable: false,
-            text: 'Email',
-            value: 'email',
-          },
-          {
-            sortable: false,
-            text: 'Title',
+            text: '제목',
             value: 'title',
           },
           {
             sortable: false,
-            text: 'Views',
+            text: '작성자',
+            value: 'nickname',
+          },
+          {
+            sortable: false,
+            text: '조회수',
             value: 'views',
             align: 'right',
           },
           {
             sortable: false,
-            text: 'Recommends',
-            value: 'recommends',
+            text: '추천수',
+            value: 'recommend',
+            align: 'right',
+          },
+          {
+            sortable: false,
+            text: '댓글수',
+            value: 'comment_cnt',
+            align: 'right',
+          },
+          {
+            sortable: false,
+            text: '작성일자',
+            value: 'writedate',
             align: 'right',
           },
         ],
-        items: [
-          {
-            nickname: 'jungho',
-            email: 'test@testemail.com',
-            posts: 0,
-            status: 'NONBLOCKED',
-          }
+        members: [
         ],
         boards: [
-          {
-            nickname: 'jungho',
-            email: 'test@testemail.com',
-            title: '첫번째 개시글',
-            views: 0,
-            recommends: 0,
-          }
         ],
         tabs: 0,
         tasks: {
@@ -527,18 +336,17 @@ import PercentageArrow from '@/components/PercentageArrow'
             },
           ],
         },
-        list: {
-          0: false,
-          1: false,
-          2: false,
-        },
       }
     },
 
     methods: {
-      complete (index) {
-        this.list[index] = !this.list[index]
+      alert(){
+        alert('clcik');
       },
+      pageNext(page){
+      let params = {'page': page, 'category': this.category}
+      this.$store.dispatch("board/pageNext", params);
+    }
     },
   }
 </script>
